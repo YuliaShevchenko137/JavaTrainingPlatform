ff515488830a8560476fd4f080ca46dc779932ce
And22And <a181096@gmail.com>
2017-05-21 19:54:59 +0300

    change initializing sql
    change id type
    add names for constraaints

---
+++ database/src/main/resources/dropDB.sql
@@ -0,0 +1,8 @@
+drop table params;
+drop table list_type;
+drop table referense_attributes;
+drop table objects;
+drop table OBJECT_TYPE_ATTRIBUTES;
+drop table attributes;
+drop table object_types;
+drop table attribute_types;
