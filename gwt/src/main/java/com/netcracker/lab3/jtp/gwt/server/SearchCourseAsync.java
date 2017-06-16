e492fbb1be3805883ca19be2b7772440144f2592
And22And <a181096@gmail.com>
2017-06-16 19:43:29 +0300

    fix gwt compile error
    change web pom
    remove gwt module
    rename web directory to webapp

--- gwt/src/main/java/com/netcracker/lab3/jtp/gwt/server/SearchCourseAsync.java
+++
@@ -1,7 +0,0 @@
-package com.netcracker.lab3.jtp.gwt.server;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface SearchCourseAsync {
-    void search(String name, AsyncCallback<String> async);
-}
